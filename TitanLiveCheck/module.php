6846c97599951e6437ee1bcae3f46dc397e7dd8c
Brelag <37905782+Brelag@users.noreply.github.com>
2019-01-29 16:22:39 +0100

    Add files via upload

--- TitanLiveCheck/module.php
+++ TitanLiveCheck/module.php
@@ -61,7 +61,7 @@
         public function LiveCheck() {
           // Sendet alle 24h einen E-Mail
 
-          SMTP_SendMail($this->GetIDForIdent("Alive"), "ALIVE: $this->GetIDForIdent("License")", "");
+          SMTP_SendMail($this->GetIDForIdent("Alive"), "ALIVE: $this->GetIDForIdent("Alive")", "");
 
         }
 
