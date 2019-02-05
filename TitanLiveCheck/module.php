d24a6888b762e5a5daca6dea869f76d43d79727e
Brelag <37905782+Brelag@users.noreply.github.com>
2019-02-05 10:59:01 +0100

    Add files via upload

--- TitanLiveCheck/module.php
+++ TitanLiveCheck/module.php
@@ -92,8 +92,9 @@
                   $ParentID = IPS_GetParent($VarIDString);
                   $ParentName = IPS_GetName($ParentID);
                   $VariableName = IPS_GetName($VarIDString);
+                  $LastUpdate = $VarInfo["VariableUpdated"];
 
-                  SMTP_SendMail($this->ReadPropertyInteger("LastUpdate"), $BetreffALERT, "$ParentName: $VariableName, $VarIDString; Letzter Update: $VarInfo["VariableUpdated"]");
+                  SMTP_SendMail($this->ReadPropertyInteger("LastUpdate"), $BetreffALERT, "$ParentName: $VariableName, $VarIDString; Letzter Update: $LastUpdate");
                 }
                 SetValue($this->GetIDForIdent("TESTString"), date("d.m.y - H:i:s", $LastUpdate));
             }
